f5c7b648eaa00cc2ce46e930770a83a40c99e95c
Salman Siddiqui <salmansiddiqui172002@gmail.com>
2025-11-25 07:47:45 +0300

    day 01

--- day-01/part-1.py
+++ day-01/part-1.py
@@ -5,7 +5,8 @@ right_list = []
 
 with Path("input.txt").open("r") as file:
     for line in file:
-        left, right = map(int, line.split())
+        numbers = line.split()
+        left, right = map(int, numbers)
         left_list.append(left)
         right_list.append(right)
 
--- day-01/part-2.py
+++ day-01/part-2.py
@@ -5,13 +5,23 @@ right_list = []
 
 with Path("input.txt").open("r") as file:
     for line in file:
-        left, right = map(int, line.split())
+        numbers = line.split()
+        left, right = map(int, numbers)
         left_list.append(left)
         right_list.append(right)
 
+
+right_count = {}
+for num in right_list:
+    if num in right_count:
+        right_count[num] += 1
+    else:
+        right_count[num] = 1
+
 similarity_score = 0
 for num in left_list:
-    count = right_list.count(num)
-    similarity_score += num * count
+    if num in right_count:
+        similarity_score += num * right_count[num]
+
 
 print(similarity_score)
